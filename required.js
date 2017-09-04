46bfe623891909ef49bd0ac760b906fdddae16d9
July <197653770@qq.com>
2017-09-04 23:48:54 +0800

    第一次提交

---
+++ required.js
@@ -0,0 +1,28 @@
+﻿define(['jquery', 'rdk'], function($,rdk){
+	return {
+		disasterInput: function(disaster_date, disaster_num, disaster_type, longitude, latitude, disaster_area, disaster_summary, disaster_detail) {
+			var json_params = {
+				'flag' : '01', 
+				'occurtime' : disaster_date,
+				'serial' : disaster_num,
+				'disastertype' : disaster_type,
+				'acquisitionsite_longitude' : longitude,
+				'acquisitionsite_latitude' : latitude,
+				'disastersite' : disaster_area,
+				'summary' : disaster_summary,
+				'disasterdetail' : disaster_detail
+			};
+			$.ajax({
+				type: 'POST',
+				contentType: 'application/json; charset=utf-8',
+				url: rdk.rdkUrl('sea_warning_input.js', 'homePage'),
+				data: json_params,
+				async: false,
+				success: functioin(data){
+					// var result = eval('(' + data.result + ')');
+					alert('提交成功');
+				}
+			});
+		}
+	}
+});
---
+++ sea_warning_input.js
@@ -0,0 +1,24 @@
+﻿(function(){
+	var columns = 'occurtime, serial, disastertype, acquisitionsite_longitude, acquisitionsite_latitude, disastersite, summary, disasterdetail';
+	function _post(request, script) {
+		var flag = request.param.flag,
+			occurtime = request.param.occurtime,
+			serial = request.param.serial,
+			disastertype = request.param.disastertype,
+			acquisitionsite_longitude = request.param.acquisitionsite_longitude,
+			acquisitionsite_latitude = request.param.acquisitionsite_latitude,
+			disastersite = request.param.disastersite,
+			summary = request.param.summary,
+			disasterdetail = request.param.disasterdetail;
+		var sql_1 = '';
+		if (flag == '01') {
+			sql_1 = "INSERT INTO oceandisaster(" + columns + ") VALUES('" + occurtime + "', '" + serial + "', '" + disastertype + "', '" 
+				+ acquisitionsite_longitude + "', '" + acquisitionsite_latitude + "', '" + disastersite + "', '" + summary + "', '" + disasterdetail + "');";
+		}
+		return Data.update(sql_1);
+	}
+
+	return {
+		post : _post
+	}
+})();
